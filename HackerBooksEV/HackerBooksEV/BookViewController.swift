8c32c448c1c4b31009df23d7dc7c234c9f72abaf
eduardo vizuete <vizuete.eduardo@gmail.com>
2017-02-04 12:45:07 -0500

    HackerBooks: BookViewController IBoutlets

--- HackerBooksEV/HackerBooksEV/BookViewController.swift
+++ HackerBooksEV/HackerBooksEV/BookViewController.swift
@@ -10,19 +10,46 @@ import UIKit
 
 class BookViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    
     let model: Book
+    
+    
+    // MARK: - Initialization
     
     init (model: Book) {
         self.model = model
         super.init(nibName: nil, bundle: nil)
+        syncViewWithModel()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - View lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        syncViewWithModel()
+    }
+    
+    // MARK: - Sync model -> View
+    func syncViewWithModel() {
+        title = model.title
+        titleLabel.text = model.title
+        //photoView.image = model.imageUrl
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func readPDF(_ sender: Any) {
+    }
     
     
-    
+    @IBAction func checkFavourite(_ sender: Any) {
+    }
 
 }
